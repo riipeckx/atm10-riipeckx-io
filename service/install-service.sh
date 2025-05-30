c33f8a52755f53c63d384f1b5e9edf122ea71979
riipeckx <riipeckx@linux.com>
2025-05-30 16:19:30 +0200

    fix: :ambulance: missing extension

--- service/install-service.sh
+++ service/install-service.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 SERVICE_NAME="atm10.riipeckx.io"
 echo 'Installing service now...'
-cp -rp $SERVICE_NAME /etc/systemd/system/$SERVICE_NAME.service
+cp -rp $SERVICE_NAME.service /etc/systemd/system/$SERVICE_NAME.service
 echo 'Service installed successfully!'
