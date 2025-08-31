3fc2c5647cca876e43331058e86a39559ce64d42
riipeckx <riipeckx@linux.com>
2025-08-31 14:29:38 +0200

    feat: :arrow_up: upgrade neoforge version to 21.1.203 in service and scripts

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.194/unix_args.txt nogui "$@"
+java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.1.203/unix_args.txt nogui "$@"
