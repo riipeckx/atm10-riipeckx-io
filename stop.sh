aaac3aa18e733202a3c484d1f2f433a69d46ef80
riipeckx <riipeckx@linux.com>
2025-09-05 12:37:39 +0200

    feat: ✨ enhance stop script with clearer countdown messages

--- stop.sh
+++ stop.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+/usr/bin/mcrcon -H localhost -P 25575 -p localhost "say Server stopping in 1 minute..."
+sleep 30
+
+/usr/bin/mcrcon -H localhost -P 25575 -p localhost "say Server stopping in 30 seconds..."
+sleep 20
+
 for i in {10..1}; do
-  /usr/bin/mcrcon -H localhost -P 25575 -p localhost "say Server stopping in $i..."
+  /usr/bin/mcrcon -H localhost -P 25575 -p localhost "say Server stopping in $i seconds..."
   sleep 1
 done
 
-/usr/bin/mcrcon -H localhost -P 25575 -p localhost "say Stopping server now..."
-sleep 1
 /usr/bin/mcrcon -H localhost -P 25575 -p localhost save-all stop
