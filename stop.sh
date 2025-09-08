ceb94a8b0fbba83799fe4a0ceb0c04c19b7105e5
riipeckx <riipeckx@linux.com>
2025-09-08 08:20:13 +0200

    feat: ✨ refactor stop script to use variable for mcrcon command

--- stop.sh
+++ stop.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-/usr/bin/mcrcon -H localhost -P 25575 -p localhost "say Server stopping in 1 minute..."
+MCRCON_CMD="/usr/bin/mcrcon -H localhost -P 25575 -p localhost"
+
+$MCRCON_CMD "say Server stopping in 1 minute..."
 sleep 30
 
-/usr/bin/mcrcon -H localhost -P 25575 -p localhost "say Server stopping in 30 seconds..."
+$MCRCON_CMD "say Server stopping in 30 seconds..."
 sleep 27
 
 for i in {3..1}; do
-  /usr/bin/mcrcon -H localhost -P 25575 -p localhost "say Server stopping in $i seconds..."
+  $MCRCON_CMD "say Server stopping in $i seconds..."
   sleep 1
 done
 
-/usr/bin/mcrcon -H localhost -P 25575 -p localhost save-all stop
+$MCRCON_CMD save-all stop
