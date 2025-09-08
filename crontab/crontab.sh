e672782d0d54701a93af764ab0f20a8239572c15
riipeckx <riipeckx@linux.com>
2025-09-08 08:13:37 +0200

    feat: ✨ update crontab entry to restart service daily at 3:59am

--- crontab/crontab.sh
+++ crontab/crontab.sh
@@ -3,7 +3,7 @@
 if crontab -l | grep -q 'atm10.riipeckx.io.service'; then
     echo "Crontab entry already exists."
 else
-    # Add the crontab entry to restart the service daily at 4am
-    (crontab -l 2>/dev/null; echo "0 4 * * * /usr/bin/systemctl restart atm10.riipeckx.io.service") | crontab -
-    echo "Crontab entry added to restart atm10.riipeckx.io.service daily at 4am."
-fi  
+    # Add the crontab entry to restart the service daily at 3:59am
+    (crontab -l 2>/dev/null; echo "59 3 * * * /usr/bin/systemctl restart atm10.riipeckx.io.service") | crontab -
+    echo "Crontab entry added to restart atm10.riipeckx.io.service daily at 3:59am."
+fi
